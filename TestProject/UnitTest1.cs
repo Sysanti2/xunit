e55939a6e7ee59c464446f2ee824cb771c763091
Elmehdi <elmehdi.ahmed.bourja@gmail.com>
2022-08-21 15:30:50 +0100

    Add Fixture Use DI Use IClassFixture

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -5,8 +5,27 @@ using xunit_project;
 
 namespace TestProject
 {
-    public class UnitTest1
+
+    public class CustomerFixture
     {
+        public Customer cstm => new Customer();
+    }
+
+    public class CalculatorFixture
+    {
+        public Calculator Calc => new Calculator();
+    }
+    public class UnitTest1 : IClassFixture<CalculatorFixture>, IClassFixture<CustomerFixture>
+    {
+        public readonly CalculatorFixture _calculatorFixture;
+        public readonly CustomerFixture _customerFixture;
+
+        public UnitTest1(CalculatorFixture calculatorFixture, CustomerFixture customerFixture)
+        {
+            _calculatorFixture = calculatorFixture;
+            _customerFixture = customerFixture;
+        }
+
         [Fact]
         [Trait ("Category", "Fibo")]
         public void Test1()
@@ -18,7 +37,7 @@ namespace TestProject
         [Trait("Category", "Fibo")]
         public void Add_GivenTwoValues_ReturnSum()
         {
-            var calc = new Calculator();
+            var calc = _calculatorFixture.Calc;
             var result = calc.add(1,2 );
             Assert.Equal(3, result);
         }
@@ -28,7 +47,7 @@ namespace TestProject
         [Trait("Category", "Fibo")]
         public void Add_GivenTwoDoubleValues_ReturnSum()
         {
-            var calc = new Calculator();
+            var calc = _calculatorFixture.Calc;
             var result = calc.addDouble(1.1, 2.2);
             Assert.Equal(3.3, result,1) ;
         }
@@ -38,7 +57,7 @@ namespace TestProject
         [Trait("Category", "Number")]
         public void Add_GivenSrtring_ReturName()
         {
-            var calc = new Calculator();
+            var calc = _calculatorFixture.Calc;
             var result = calc.ReturnString("Hello");
             Assert.Contains("he", result, StringComparison.OrdinalIgnoreCase);
             Assert.DoesNotContain("he", result);
@@ -52,7 +71,7 @@ namespace TestProject
         [Trait("Category", "Number")]
         public void Add_GivenBool_ReturStatus()
         {
-            var calc = new Calculator();
+            var calc = _calculatorFixture.Calc;
             var result = calc.ReturnNull();
             Assert.Null(result);
             Assert.True(string.IsNullOrEmpty(result));
@@ -63,7 +82,7 @@ namespace TestProject
         [Trait("Category", "Fibo")]
         public void FiboNotIncludeZero ()
         {
-            var calc = new Calculator();
+            var calc = _calculatorFixture.Calc;
             Assert.All(calc.Fibo , n => Assert.NotEqual(0,n));
         }
 
@@ -72,7 +91,7 @@ namespace TestProject
         [Trait("Category", "Fibo")]
         public void checkFibo ()
         {
-            var calc = new Calculator();
+            var calc = _calculatorFixture.Calc;
             var testcollection =new List<int> { 1, 1, 2, 3, 5, 8, 13 };
             Assert.Equal(calc.Fibo, testcollection);
         }
@@ -81,7 +100,7 @@ namespace TestProject
         [Trait("Category", "string")]
         public void CheckNameNotEmpty()
         {
-            var cotsomer = new Customer();
+            var cotsomer = _customerFixture.cstm;
             Assert.NotEmpty(cotsomer.Name);
             Assert.False(string.IsNullOrEmpty(cotsomer.Name));
 
@@ -93,7 +112,7 @@ namespace TestProject
         [Trait("Category", "Customer")]
         public void checklegidorDiscount()
         {
-            var ctm = new Customer();
+            var ctm = _customerFixture.cstm;
             Assert.InRange(ctm.Age, 20, 35);
         }
 
@@ -101,7 +120,7 @@ namespace TestProject
         [Trait("Category", "Customer")]
         public void GetOrderByNameNotNull()
         {
-            var cstm = new Customer();
+            var cstm = _customerFixture.cstm;
             var exception =  Assert.Throws<ArgumentException>(() => cstm.getOrderByName(null));
         }
 
